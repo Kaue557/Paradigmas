43f0820ab42cbaa2027fa6759047fcf2646da10d
Kaue557 <kauelima557@gmail.com>
2026-09-18 17:32:45 -0300

    exercicios videoaula 5-8

--- exercicios_aula1/exercicios_videoaula.lisp
+++ exercicios_aula1/exercicios_videoaula.lisp
@@ -46,4 +46,53 @@
             ; colocar a cabeça de volta (cons) no começo
             (t (cons (car lst) (delpri elt (cdr lst)))))))
 
-(write (delpri 4 '(1 3 4 4 5 6)))
+; (write (delpri 4 '(1 3 4 4 5 6)))
+; (format t "~%")
+
+; DELETA TODAS AS OCORRÊNCIAS
+(defun delall (elt lst)
+    (progn ; parada do macro, ignorar
+        (deb lst) ; parada do macro, ignorar
+        (cond
+            ((null lst) nil)
+            ; "se o elemento (elt) for igual a cabeça da lista, pula ele e não coloca no resultado -> (delall elt (cdr lst))"
+            ((eql elt (car lst)) (delall elt (cdr lst)))
+            ; senão, coloca o valor (que nao será deletado) na cabeça do resultado e chama a função recursivamente
+            (t (cons (car lst) (delall elt (cdr lst)))))))
+
+; (write (delall 5 '(3 5 6 4 5 5 7 8 2 5 5)))
+; (format t "~%")
+
+; INVERTER A LISTA
+; NAO EXISTE EM LISP CONCATENAÇÃO (APPEND) ENTRE LISTA E ELEMENTO, APENAS ENTRE LISTAS
+(defun inverte (lst) ; esse é o jeito vagabundo e fazer, o correto é com acumulador 
+    (cond
+        ((null lst) nil)
+        ; inverte o restante da lista e coloca o cabeça no final
+        (t (append (inverte (cdr lst))
+                    (list (car lst))))))
+
+; (write (inverte '(a b c d e)))
+; (format t "~%")
+
+; PRIMEIROS NATURAIS
+(defun n_nat (n) (n_nat* 1 n)) ; n_nat* é o nome do prog. auxiliar (recebe 1(i) e 'n'), nada a ver com ponteiro!!
+
+(defun n_nat* (i n) ; 'i' começa em 1, definido lá em cima
+    (cond
+        ((> i n) nil)
+        (t (cons i (n_nat* (+ i 1) n)))))
+    
+; (write (n_nat 30))
+; (format t "~%")
+
+; ATÉ O K-ÉSIMO (testando organização diferente de código)
+(defun ate (k lst)
+    (cond
+        ((or (null lst) (eql k 0)) nil)
+        (t
+            (cons
+                (car lst)
+                (ate (- k 1) (cdr lst))))))
+
+(write (ate 2 '(3 2 1)))
